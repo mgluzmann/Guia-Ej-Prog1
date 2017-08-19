a4d29c1e37f9051af23f8d929201dbaf29b75cf6
mgluzmann <lapiceraotorrino>
2017-08-19 16:07:47 -0300

    Actualizo

---
+++ Ejercicio 15/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //15.Escribir en  y codificar en C un programa que muestre los múltiplos de 6 comprendidos entre 0 y 100
+
+    for (int i = 6 ; i <= 100 ; i=i+6)
+    {
+        printf("%d\n",i);
+    }
+}
