9137e15c2e586499ba791a0179a4f8c831f36e03
alumno <alumno>
2017-08-26 21:33:56 -0300

    ejercicios de clase y funciones

---
+++ FnSumar1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumar1 (int num1, int num2); //prototipo de la funcion, lo usa el compilador
+
+int main()//aca hago la llamada a la funcion, es la parte que toma los datos.
+{
+
+sumar1( , )
+
+
+
+
+
+}
+int sumar1 (int num1, int num2)//primero se hace esto. cuerpo de la funcion sin punto y coma. Es la parte que OPERA.
+{
+
+int suma;//igual q la funcion
+
+suma = num1 + num 2;
+
+return suma;//retorno la variable del mismo tipo
+}
---
+++ funcionObtenerMayor/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int obtenerMayor(int, int, int);
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+    int mayor;
+/*
+      printf("Ingrese un numero: ");
+      scanf("%d",&num1);
+
+      printf("Ingrese un numero: ");
+      scanf("%d",&num2);
+
+      printf("Ingrese un numero: ");
+      scanf("%d",&num3);
+*/
+      mayor = obtenerMayor(10,10,2);
+
+      printf("\n\nEl mayor numero ingresado es: %d",mayor);
+
+    return 0;
+}
+int obtenerMayor(int num1, int num2, int num3){
+
+    int mayor;
+
+    if(num1 >= num2 && num1 >= num3)
+    {
+        mayor = num1;
+    }
+    else if(num2 >= num3 && num2 >= num1)
+    {
+        mayor = num2;
+    }
+    else
+    {
+        mayor = num3;
+    }
+
+    return mayor;
+}
+
